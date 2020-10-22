b253a346219d916795ea49ac3b25ad6e9d85e65f
Solomon Wiznitzer <shlomo.wiz@gmail.com>
2020-10-22 18:16:11 -0500

    fix remote install bug

--- interbotix_ros_xsarms/install/xsarm_remote_install.sh
+++ interbotix_ros_xsarms/install/xsarm_remote_install.sh
@@ -23,6 +23,7 @@ if [ ! -d "$INTERBOTIX_WS/src" ]; then
   mkdir -p $INTERBOTIX_WS/src
   cd $INTERBOTIX_WS
   catkin_make
+  cd src
   git clone https://github.com/Interbotix/interbotix_ros_manipulators.git
   cd interbotix_ros_manipulators
   git checkout $ROS_NAME
