ee09c22219fc10b0883e2fa292998332290af72d
Luke Schmitt <luke@trossenrobotics.com>
2022-04-07 21:09:37 -0500

    [ros_xsarms] Major MATLAB setup refactor
    Remove Python check as Python is no longer needed in the MATLAB-ROS API.
    Setup script returns false if any step fails. After adding a component's
    path, now checks if import works properly. Minor changes to
    build_ros1_messages script.

--- interbotix_ros_xsarms/examples/matlab_demos/interbotix_build_ros1_messages.m
+++ interbotix_ros_xsarms/examples/matlab_demos/interbotix_build_ros1_messages.m
@@ -1,8 +1,10 @@
 function success = interbotix_build_ros1_messages(options)
     % Run this function to build the Interbotix ROS Messages for use with MATLAB
     arguments
-        options.py27path string = "/usr/bin/python2.7"
+        options.py27path string = "/usr/bin/python2.7" % absolute path to python2.7 executable
     end
+
+    success = false;
 
     rel_core_msgs_path = "../../../../interbotix_ros_core/interbotix_ros_xseries/matlab_msg_gen_ros1/";
 
@@ -21,6 +23,7 @@ function success = interbotix_build_ros1_messages(options)
         rehash toolboxcache
         if any(strcmp(rosmsg("list"), "interbotix_xs_msgs/JointSingleCommand"))
             disp("Done building messages. You can now run MATLAB scripts using the messages.")
+            disp("Make sure to restart MATLAB before running any scripts.")
             success = true;
         else
             warning("Something went wrong when building messages.")
@@ -30,7 +33,4 @@ function success = interbotix_build_ros1_messages(options)
         disp("Messages already built. Skipping...")
         success = true;
     end
-
-    disp("Make sure to restart MATLAB before running any scripts.")
-
 end
