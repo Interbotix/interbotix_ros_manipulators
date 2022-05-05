9cdf83dcf6441525270015181f12d8915aed55a8
Luke Schmitt <luke@trossenrobotics.com>
2022-05-05 13:00:00 -0500

    [xsarm_descriptions] Package refactor
    Refactored URDFs. Styled URDFs. Renamed mesh files. Added and enforced
    linting. Modified RViz configuration file.

--- interbotix_ros_xsarms/interbotix_xsarm_descriptions/launch/remote_view.launch.py
+++ interbotix_ros_xsarms/interbotix_xsarm_descriptions/launch/remote_view.launch.py
@@ -1,50 +1,69 @@
-from ament_index_python.packages import get_package_share_directory
+# Copyright 2022 Trossen Robotics
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import (
     LaunchConfiguration,
     PathJoinSubstitution,
-    TextSubstitution,
 )
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
     declared_arguments = [
         DeclareLaunchArgument(
-            "robot_name",
-            default_value=TextSubstitution(text=""),
+            'robot_name',
             description=(
-                "name of the robot (typically equal to robot_model, but could be "
-                "anything)'"
+                'name of the robot (typically equal to `robot_model`, but could be anything)'
             ),
         ),
         DeclareLaunchArgument(
-            "rvizconfig",
-            default_value=TextSubstitution(text=""),
-            description="file path to the config file RViz should load",
+            'rvizconfig',
+            default_value=PathJoinSubstitution([
+                FindPackageShare('interbotix_xsarm_descriptions'),
+                'rviz',
+                'xsarm_description.rviz'
+            ]),
+            description='file path to the config file RViz should load',
         ),
     ]
 
-    xsarm_descriptions_prefix = get_package_share_directory(
-        "interbotix_xsarm_descriptions"
-    )
-
-    robot_name = LaunchConfiguration("robot_name")
-    rvizconfig = LaunchConfiguration("rvizconfig")
-
-    rvizconfig = PathJoinSubstitution(
-        [xsarm_descriptions_prefix, "rviz", "xsarm_description.rviz"]
-    )
-
     rviz2_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        namespace=robot_name,
-        name="rviz2",
-        output="log",
-        arguments=["-d", rvizconfig],
+        package='rviz2',
+        executable='rviz2',
+        namespace=LaunchConfiguration('robot_name'),
+        name='rviz2',
+        output='log',
+        arguments=[
+            '-d', LaunchConfiguration('rvizconfig')
+        ],
     )
 
     nodes_to_start = [
--- interbotix_ros_xsarms/interbotix_xsarm_descriptions/launch/xsarm_description.launch.py
+++ interbotix_ros_xsarms/interbotix_xsarm_descriptions/launch/xsarm_description.launch.py
@@ -1,99 +1,101 @@
+# Copyright 2022 Trossen Robotics
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from interbotix_xs_modules.xs_launch import (
+    declare_interbotix_xsarm_robot_description_launch_arguments,
+    get_interbotix_xsarm_models,
+)
 from launch import LaunchDescription
-from launch.conditions import IfCondition
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.conditions import IfCondition
 from launch.substitutions import (
-    Command,
-    FindExecutable,
     LaunchConfiguration,
     PathJoinSubstitution,
-    TextSubstitution,
 )
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
 def launch_setup(context, *args, **kwargs):
-    robot_model_launch_arg = LaunchConfiguration("robot_model")
-    robot_name_launch_arg = LaunchConfiguration("robot_name")
-    base_link_frame_launch_arg = LaunchConfiguration("base_link_frame")
-    show_ar_tag_launch_arg = LaunchConfiguration("show_ar_tag")
-    show_gripper_bar_launch_arg = LaunchConfiguration("show_gripper_bar")
-    show_gripper_fingers_launch_arg = LaunchConfiguration("show_gripper_fingers")
-    use_world_frame_launch_arg = LaunchConfiguration("use_world_frame")
-    external_urdf_loc_launch_arg = LaunchConfiguration("external_urdf_loc")
-    use_rviz_launch_arg = LaunchConfiguration("use_rviz")
-    load_gazebo_configs_launch_arg = LaunchConfiguration("load_gazebo_configs")
-    use_joint_pub_launch_arg = LaunchConfiguration("use_joint_pub")
-    use_joint_pub_gui_launch_arg = LaunchConfiguration("use_joint_pub_gui")
-    rviz_config_launch_arg = LaunchConfiguration("rvizconfig")
-    model_launch_arg = LaunchConfiguration("model")
-
-    rviz_config_launch_arg = PathJoinSubstitution(
-        [
-            FindPackageShare("interbotix_xsarm_descriptions"), 
-            "rviz",
-            "xsarm_description.rviz",
-        ]
-    )
-
-    urdf_path = PathJoinSubstitution(
-        [
-            FindPackageShare("interbotix_xsarm_descriptions"),
-            "urdf",
-            robot_model_launch_arg
-        ]
-    )
-
-    model_launch_arg = Command(
-        [
-            FindExecutable(name="xacro"), " ", urdf_path, ".urdf.xacro ",
-            "robot_name:=", robot_name_launch_arg, " ",
-            "base_link_frame:=", base_link_frame_launch_arg, " ",
-            "show_ar_tag:=", show_ar_tag_launch_arg, " ",
-            "show_gripper_bar:=", show_gripper_bar_launch_arg, " ",
-            "show_gripper_fingers:=", show_gripper_fingers_launch_arg, " ",
-            "use_world_frame:=", use_world_frame_launch_arg, " ",
-            "external_urdf_loc:=", external_urdf_loc_launch_arg, " ",
-            "load_gazebo_configs:=", load_gazebo_configs_launch_arg, " ",
-        ]
-    )
+    robot_name_launch_arg = LaunchConfiguration('robot_name')
+    use_rviz_launch_arg = LaunchConfiguration('use_rviz')
+    use_joint_pub_launch_arg = LaunchConfiguration('use_joint_pub')
+    use_joint_pub_gui_launch_arg = LaunchConfiguration('use_joint_pub_gui')
+    use_sim_time_launch_arg = LaunchConfiguration('use_sim_time')
+    rviz_config_launch_arg = LaunchConfiguration('rvizconfig')
+    robot_description_launch_arg = LaunchConfiguration('robot_description')
 
     robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        parameters=[
-            {
-                "robot_description": model_launch_arg
-            }
-        ],
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{
+            'robot_description': robot_description_launch_arg,
+            'use_sim_time': use_sim_time_launch_arg,
+        }],
         namespace=robot_name_launch_arg,
-        output={"both": "log"},
+        output={'both': 'log'},
     )
 
     joint_state_publisher_node = Node(
         condition=IfCondition(use_joint_pub_launch_arg),
-        package="joint_state_publisher",
-        executable="joint_state_publisher",
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
         namespace=robot_name_launch_arg,
-        output={"both": "log"},
+        parameters=[{
+            'use_sim_time': use_sim_time_launch_arg,
+        }],
+        output={'both': 'log'},
     )
 
     joint_state_publisher_gui_node = Node(
         condition=IfCondition(use_joint_pub_gui_launch_arg),
-        package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui",
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
         namespace=robot_name_launch_arg,
-        output={"both": "log"},
+        parameters=[{
+            'use_sim_time': use_sim_time_launch_arg,
+        }],
+        output={'both': 'log'},
     )
 
     rviz2_node = Node(
         condition=IfCondition(use_rviz_launch_arg),
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
         namespace=robot_name_launch_arg,
-        arguments=["-d", rviz_config_launch_arg],
-        output={"both": "log"},
+        arguments=[
+            '-d', rviz_config_launch_arg,
+        ],
+        parameters=[{
+            'use_sim_time': use_sim_time_launch_arg,
+        }],
+        output={'both': 'log'},
     )
 
     return [
@@ -105,132 +107,71 @@ def launch_setup(context, *args, **kwargs):
 
 
 def generate_launch_description():
-    return LaunchDescription(
-        [
-            DeclareLaunchArgument(
-                "robot_model",
-                default_value=TextSubstitution(text=""),
-                choices=(
-                    "px100",
-                    "px150",
-                    "rx150",
-                    "rx200",
-                    "wx200",
-                    "wx250",
-                    "wx250s",
-                    "vx250",
-                    "vx300",
-                    "vx300s",
-                    "mobile_px100",
-                    "mobile_wx200",
-                    "mobile_wx250s",
-                ),
-                description=(
-                    "model type of the Interbotix Arm such as 'wx200' or 'rx150'"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "robot_name",
-                default_value=LaunchConfiguration("robot_model"),
-                description=(
-                    "name of the robot (typically equal to robot_model, but could be "
-                    "anything)'"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "base_link_frame",
-                default_value=TextSubstitution(text="base_link"),
-                description=(
-                    "name of the 'root' link on the arm; typically 'base_link', but "
-                    "can be changed if attaching the arm to a mobile base that "
-                    "already has a 'base_link' frame"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "show_ar_tag",
-                default_value="false",
-                description=(
-                    "if true, the AR tag mount is included in the 'robot_description'"
-                    " parameter; if false, it is left out; set to true if using the "
-                    "AR tag mount in your project"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "show_gripper_bar",
-                default_value="true",
-                description=(
-                    "if true, the gripper_bar link is included in the "
-                    "'robot_description' parameter; if false, the gripper_bar and "
-                    "finger links are not loaded. Set to false if you have a custom "
-                    "gripper attachment"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "show_gripper_fingers",
-                default_value="true",
-                description=(
-                    "if true, the gripper fingers are included in the "
-                    "'robot_description' parameter; if false, the gripper "
-                    "finger links are not loaded. Set to false if you have "
-                    "custom gripper fingers"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "use_world_frame",
-                default_value="true",
-                description=(
-                    "set this to true if you would like to load a 'world' frame "
-                    "to the 'robot_description' parameter which is located exactly "
-                    "at the 'base_link' frame of the robot; if using multiple "
-                    "robots or if you would like to attach the 'base_link' frame of "
-                    "the robot to a different frame, set this to false"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "external_urdf_loc",
-                default_value=TextSubstitution(text=""),
-                description=(
-                    "the file path to the custom urdf.xacro file that you would like "
-                    "to include in the Interbotix robot's urdf.xacro file"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "use_rviz",
-                default_value="true",
-                description="launches Rviz",
-            ),
-            DeclareLaunchArgument(
-                "load_gazebo_configs",
-                default_value="false",
-                description=(
-                    "set this to true if Gazebo is being used; it makes sure to "
-                    "include Gazebo related configs in the 'robot_description' "
-                    "parameter so that the robot models show up black in Gazebo"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "use_joint_pub",
-                default_value="false",
-                description="launches the joint_state_publisher node",
-            ),
-            DeclareLaunchArgument(
-                "use_joint_pub_gui",
-                default_value="false",
-                description="launches the joint_state_publisher GUI",
-            ),
-            DeclareLaunchArgument(
-                "rvizconfig",
-                default_value=TextSubstitution(text=""),
-                description="file path to the config file Rviz should load",
-            ),
-            DeclareLaunchArgument(
-                "model",
-                default_value=TextSubstitution(text=""),
-                description=(
-                    "file path to the robot-specific URDF including arguments to be "
-                    "passed in"
-                ),
-            ),
-            OpaqueFunction(function=launch_setup),
-        ]
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'robot_model',
+            choices=get_interbotix_xsarm_models(),
+            description="model type of the Interbotix Arm such as 'wx200' or 'rx150'",
+        )
     )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'robot_name',
+            default_value=LaunchConfiguration('robot_model'),
+            description=(
+                'name of the robot (typically equal to `robot_model`, but could be anything)'
+            ),
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_rviz',
+            default_value='true',
+            choices=('true', 'false'),
+            description='launches RViz if set to true',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_joint_pub',
+            default_value='false',
+            choices=('true', 'false'),
+            description='launches the joint_state_publisher node',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_joint_pub_gui',
+            default_value='false',
+            choices=('true', 'false'),
+            description='launches the joint_state_publisher GUI',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'rvizconfig',
+            default_value=PathJoinSubstitution([
+                FindPackageShare('interbotix_xsarm_descriptions'),
+                'rviz',
+                'xsarm_description.rviz',
+            ]),
+            description='file path to the config file RViz should load',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            choices=('true', 'false'),
+            description=(
+                'tells ROS nodes asking for time to get the Gazebo-published simulation time, '
+                'published over the ROS topic /clock'
+            )
+        )
+    )
+    declared_arguments.extend(
+        declare_interbotix_xsarm_robot_description_launch_arguments(),
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
