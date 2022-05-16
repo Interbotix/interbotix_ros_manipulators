10c710cb8c2f4b302f39b298e43d4913aef0d03d
Luke Schmitt <luke@trossenrobotics.com>
2022-05-16 16:38:13 -0500

    [xsarm_ros_control] Fully port ros_control package

--- interbotix_ros_xsarms/interbotix_xsarm_ros_control/launch/xsarm_ros_control.launch.py
+++ interbotix_ros_xsarms/interbotix_xsarm_ros_control/launch/xsarm_ros_control.launch.py
@@ -1,14 +1,49 @@
+# Copyright 2022 Trossen Robotics
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from typing import List
+
+from interbotix_xs_modules.xs_common import (
+    get_interbotix_xsarm_models,
+)
+from interbotix_xs_modules.xs_launch import (
+    declare_interbotix_xsarm_robot_description_launch_arguments,
+)
+from interbotix_xs_modules.xs_launch.xs_launch import determine_use_sim_time_param
 from launch import LaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import (
     DeclareLaunchArgument,
     IncludeLaunchDescription,
     OpaqueFunction,
 )
+from launch.conditions import LaunchConfigurationEquals
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import (
-    Command,
-    FindExecutable,
-    TextSubstitution,
     LaunchConfiguration,
     PathJoinSubstitution,
 )
@@ -18,250 +53,188 @@ from launch_ros.substitutions import FindPackageShare
 
 def launch_setup(context, *args, **kwargs):
 
-    robot_model_launch_arg = LaunchConfiguration("robot_model")
-    robot_name_launch_arg = LaunchConfiguration("robot_name")
-    base_link_frame_launch_arg = LaunchConfiguration("base_link_frame")
-    show_ar_tag_launch_arg = LaunchConfiguration("show_ar_tag")
-    show_gripper_bar_launch_arg = LaunchConfiguration("show_gripper_bar")
-    show_gripper_fingers_launch_arg = LaunchConfiguration("show_gripper_fingers")
-    use_world_frame_launch_arg = LaunchConfiguration("use_world_frame")
-    external_urdf_loc_launch_arg = LaunchConfiguration("external_urdf_loc")
-    load_gazebo_configs_launch_arg = LaunchConfiguration("load_gazebo_configs")
-    use_rviz_launch_arg = LaunchConfiguration("use_rviz")
-    mode_configs_launch_arg = LaunchConfiguration("mode_configs")
-    dof_launch_arg = LaunchConfiguration("dof") # TODO
-    model_launch_arg = LaunchConfiguration("model")
+    robot_model_launch_arg = LaunchConfiguration('robot_model')
+    robot_name_launch_arg = LaunchConfiguration('robot_name')
+    use_rviz_launch_arg = LaunchConfiguration('use_rviz')
+    mode_configs_launch_arg = LaunchConfiguration('mode_configs')
+    robot_description_launch_arg = LaunchConfiguration('robot_description')
+    hardware_type_launch_arg = LaunchConfiguration('hardware_type')
 
-    mode_configs_launch_arg = PathJoinSubstitution(
-        [
-            FindPackageShare("interbotix_xsarm_ros_control"),
-            "config",
-            "modes.yaml",
-        ]
+    # sets use_sim_time parameter to 'true' if using gazebo hardware
+    use_sim_time_param = determine_use_sim_time_param(
+        context=context,
+        hardware_type_launch_arg=hardware_type_launch_arg
     )
 
-    urdf_path = PathJoinSubstitution(
-        [
-            FindPackageShare("interbotix_xsarm_descriptions"),
-            "urdf",
-            robot_model_launch_arg,
-        ]
-    )
-
-    model_launch_arg = Command(
-        [
-            FindExecutable(name="xacro"), " ", urdf_path, ".urdf.xacro ",
-            "robot_name:=", robot_name_launch_arg, " ",
-            "base_link_frame:=", base_link_frame_launch_arg, " ",
-            "show_ar_tag:=", show_ar_tag_launch_arg, " ",
-            "show_gripper_bar:=", show_gripper_bar_launch_arg, " ",
-            "show_gripper_fingers:=", show_gripper_fingers_launch_arg, " ",
-            "use_world_frame:=", use_world_frame_launch_arg, " ",
-            "external_urdf_loc:=", external_urdf_loc_launch_arg, " ",
-            "load_gazebo_configs:=", load_gazebo_configs_launch_arg, " ",
-        ]
-    )
-
-    robot_description = {"robot_description": model_launch_arg}
-
-    controllers_config = PathJoinSubstitution(
-        [
-            FindPackageShare("interbotix_xsarm_ros_control"),
-            "config",
-            "controllers",
-            f"{robot_model_launch_arg.perform(context)}_controllers.yaml",
-        ]
-    )
-
-    hardware_config = PathJoinSubstitution(
-        [
-            FindPackageShare("interbotix_xsarm_ros_control"),
-            "config",
-            "hardware.yaml",
-        ]
-    )
+    ros2_control_controllers_config = PathJoinSubstitution([
+        FindPackageShare('interbotix_xsarm_ros_control'),
+        'config',
+        'controllers',
+        f'{robot_model_launch_arg.perform(context)}_controllers.yaml',
+    ])
 
     xsarm_control_launch_include = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                PathJoinSubstitution(
-                    [
-                        FindPackageShare("interbotix_xsarm_control"),
-                        "launch",
-                        "xsarm_control.launch.py",
-                    ]
-                )
-            ]
-        ),
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('interbotix_xsarm_control'),
+                'launch',
+                'xsarm_control.launch.py',
+            ])
+        ]),
         launch_arguments={
-            "robot_model": robot_model_launch_arg,
-            "robot_name": robot_name_launch_arg,
-            "base_link_frame": base_link_frame_launch_arg,
-            "show_ar_tag": show_ar_tag_launch_arg,
-            "show_gripper_bar": show_gripper_bar_launch_arg,
-            "show_gripper_fingers": show_gripper_fingers_launch_arg,
-            "use_world_frame": use_world_frame_launch_arg,
-            "external_urdf_loc": external_urdf_loc_launch_arg,
-            "use_rviz": use_rviz_launch_arg,
-            "mode_configs": mode_configs_launch_arg,
+            'robot_model': robot_model_launch_arg,
+            'robot_name': robot_name_launch_arg,
+            'use_rviz': use_rviz_launch_arg,
+            'mode_configs': mode_configs_launch_arg,
+            'hardware_type': hardware_type_launch_arg,
+            'use_sim_time': use_sim_time_param,
         }.items(),
+        condition=LaunchConfigurationEquals(
+            launch_configuration_name='hardware_type',
+            expected_value='actual'
+        ),
     )
 
-    # TODO: load hardware params
+    xsarm_descriptions_launch_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('interbotix_xsarm_descriptions'),
+                'launch',
+                'xsarm_description.launch.py',
+            ])
+        ]),
+        launch_arguments={
+            'robot_model': robot_model_launch_arg,
+            'robot_name': robot_name_launch_arg,
+            'use_rviz': use_rviz_launch_arg,
+            'mode_configs': mode_configs_launch_arg,
+            'hardware_type': hardware_type_launch_arg,
+            'use_sim_time': use_sim_time_param,
+        }.items(),
+        condition=LaunchConfigurationEquals(
+            launch_configuration_name='hardware_type',
+            expected_value='fake'
+        ),
+    )
+
     controller_manager_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
+        package='controller_manager',
+        executable='ros2_control_node',
         namespace=robot_name_launch_arg,
         parameters=[
-            robot_description,
-            controllers_config,
-            hardware_config,
+            {'robot_description': robot_description_launch_arg},
+            ros2_control_controllers_config,
         ],
-        output={"both": "screen"},
+        output={'both': 'screen'},
     )
 
     spawn_arm_controller_node = Node(
-        name="arm_controller_spawner",
-        package="controller_manager",
-        executable="spawner",
+        name='arm_controller_spawner',
+        package='controller_manager',
+        executable='spawner',
         namespace=robot_name_launch_arg,
         arguments=[
-            "-c",
-            f"/{robot_name_launch_arg.perform(context)}/controller_manager",
-            "arm_controller",
+            '-c',
+            f'/{robot_name_launch_arg.perform(context)}/controller_manager',
+            'arm_controller',
         ],
+        output={'both': 'screen'},
     )
 
     spawn_gripper_controller_node = Node(
-        name="gripper_controller_spawner",
-        package="controller_manager",
-        executable="spawner",
+        name='gripper_controller_spawner',
+        package='controller_manager',
+        executable='spawner',
         namespace=robot_name_launch_arg,
         arguments=[
-            "-c",
-            f"/{robot_name_launch_arg.perform(context)}/controller_manager",
-            "gripper_controller",
+            '-c',
+            f'/{robot_name_launch_arg.perform(context)}/controller_manager',
+            'gripper_controller',
         ],
+        output={'both': 'screen'},
+    )
+
+    spawn_joint_state_broadcaster_node = Node(
+        name='joint_state_broadcaster_spawner',
+        package='controller_manager',
+        executable='spawner',
+        namespace=robot_name_launch_arg,
+        arguments=[
+            '-c',
+            f'/{robot_name_launch_arg.perform(context)}/controller_manager',
+            'joint_state_broadcaster',
+        ],
+        condition=LaunchConfigurationEquals(
+            launch_configuration_name='hardware_type',
+            expected_value='fake'
+        ),
+        output={'both': 'screen'},
     )
 
     return [
         controller_manager_node,
         spawn_arm_controller_node,
         spawn_gripper_controller_node,
+        spawn_joint_state_broadcaster_node,
         xsarm_control_launch_include,
+        xsarm_descriptions_launch_include,
     ]
 
 
 def generate_launch_description():
-    return LaunchDescription(
-        [
-            DeclareLaunchArgument(
-                "robot_model",
-                default_value=TextSubstitution(text=""),
-                choices=(
-                    "px100",
-                    "px150",
-                    "rx150",
-                    "rx200",
-                    "wx200",
-                    "wx250",
-                    "wx250s",
-                    "vx250",
-                    "vx300",
-                    "vx300s",
-                    "mobile_px100",
-                    "mobile_wx200",
-                    "mobile_wx250s",
-                ),
-                description=(
-                    "model type of the Interbotix Arm such as 'wx200' or 'rx150'"
-                ),
+    declared_arguments: List[DeclareLaunchArgument] = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'robot_model',
+            choices=get_interbotix_xsarm_models(),
+            description=(
+                "model type of the Interbotix Arm such as 'wx200' or 'rx150'"
             ),
-            DeclareLaunchArgument(
-                "robot_name",
-                default_value=LaunchConfiguration("robot_model"),
-                description=(
-                    "name of the robot (typically equal to robot_model, but could be "
-                    "anything)'"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "base_link_frame",
-                default_value=TextSubstitution(text="base_link"),
-                description=(
-                    "name of the 'root' link on the arm; typically 'base_link', but "
-                    "can be changed if attaching the arm to a mobile base that "
-                    "already has a 'base_link' frame"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "show_ar_tag",
-                default_value="false",
-                description=(
-                    "if true, the AR tag mount is included in the 'robot_description'"
-                    " parameter; if false, it is left out; set to true if using the "
-                    "AR tag mount in your project"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "show_gripper_bar",
-                default_value="true",
-                description=(
-                    "if true, the gripper_bar link is included in the "
-                    "'robot_description' parameter; if false, the gripper_bar and "
-                    "finger links are not loaded. Set to false if you have a custom "
-                    "gripper attachment"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "show_gripper_fingers",
-                default_value="true",
-                description=(
-                    "if true, the gripper fingers are included in the "
-                    "'robot_description' parameter; if false, the gripper "
-                    "finger links are not loaded. Set to false if you have "
-                    "custom gripper fingers"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "use_world_frame",
-                default_value="true",
-                description=(
-                    "set this to true if you would like to load a 'world' frame "
-                    "to the 'robot_description' parameter which is located exactly "
-                    "at the 'base_link' frame of the robot; if using multiple "
-                    "robots or if you would like to attach the 'base_link' frame of "
-                    "the robot to a different frame, set this to false"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "external_urdf_loc",
-                default_value=TextSubstitution(text=""),
-                description=(
-                    "the file path to the custom urdf.xacro file that you would like "
-                    "to include in the Interbotix robot's urdf.xacro file"
-                ),
-            ),
-            DeclareLaunchArgument(
-                "use_rviz",
-                default_value="false",
-                description="launches RViz",
-            ),
-            DeclareLaunchArgument(
-                "load_gazebo_configs",
-                default_value="false",
-                description="set this to true if Gazebo is being used; it makes sure to include Gazebo related configs in the 'robot_description' parameter so that the robot models show up black in Gazebo",
-            ),
-            DeclareLaunchArgument(
-                "model",
-                default_value='""',
-                description="file path to the robot-specific URDF including arguments to be passed in",
-            ),
-            DeclareLaunchArgument(
-                "dof",
-                default_value="5",
-                description="the degrees of freedom of the arm; while the majority of the arms have 5 dof, others have 4 or 6 dof",
-            ),
-            OpaqueFunction(function=launch_setup),
-        ]
+        )
     )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'robot_name',
+            default_value=LaunchConfiguration('robot_model'),
+            description=(
+                'name of the robot (typically equal to `robot_model`, but could be anything)'
+            ),
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_rviz',
+            default_value='false',
+            description='launches RViz if set to true',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'mode_configs',
+            default_value=PathJoinSubstitution([
+                FindPackageShare('interbotix_xsarm_ros_control'),
+                'config',
+                'modes.yaml',
+            ]),
+            description="the file path to the 'mode config' YAML file",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            choices=('true', 'false'),
+            description=(
+                'tells ROS nodes asking for time to get the Gazebo-published simulation time, '
+                "published over the ROS topic /clock; this value is automatically set to 'true' if"
+                ' using Gazebo hardware'
+            )
+        )
+    )
+    declared_arguments.extend(
+        declare_interbotix_xsarm_robot_description_launch_arguments(
+            hardware_type='actual'
+        )
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
