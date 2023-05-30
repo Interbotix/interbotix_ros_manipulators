120c32a0e557fb1e2142051787b47edeea57d99a
Luke Schmitt <luke@trossenrobotics.com>
2023-05-30 16:44:44 -0500

    [ros_xsarms] Quit some demos if arm under 5 DOF
    [skip ci]

--- interbotix_ros_xsarms/examples/python_demos/bartender.py
+++ interbotix_ros_xsarms/examples/python_demos/bartender.py
@@ -1,13 +1,21 @@
 from interbotix_xs_modules.arm import InterbotixManipulatorXS
 import numpy as np
+import sys
 
 # This script makes the end-effector perform pick, pour, and place tasks
+# Note that this script may not work for every arm as it was designed for the wx250
+# Make sure to adjust commanded joint positions and poses as necessary
 #
 # To get started, open a terminal and type 'roslaunch interbotix_xsarm_control xsarm_control.launch robot_model:=wx250'
 # Then change to this directory and type 'python bartender.py  # python3 bartender.py if using ROS Noetic'
 
 def main():
     bot = InterbotixManipulatorXS("wx250", "arm", "gripper")
+
+    if (bot.arm.group_info.num_joints < 5):
+        print('This demo requires the robot to have at least 5 joints!')
+        sys.exit()
+
     bot.arm.set_ee_pose_components(x=0.3, z=0.2)
     bot.arm.set_single_joint_position("waist", np.pi/2.0)
     bot.gripper.open()
--- interbotix_ros_xsarms/examples/python_demos/ee_cartesian_trajectory.py
+++ interbotix_ros_xsarms/examples/python_demos/ee_cartesian_trajectory.py
@@ -1,12 +1,20 @@
 from interbotix_xs_modules.arm import InterbotixManipulatorXS
+import sys
 
 # This script makes the end-effector draw a square in 3D space
+# Note that this script may not work for every arm as it was designed for the wx250
+# Make sure to adjust commanded joint positions and poses as necessary
 #
 # To get started, open a terminal and type 'roslaunch interbotix_xsarm_control xsarm_control.launch robot_model:=wx250'
 # Then change to this directory and type 'python ee_cartesian_trajectory.py  # python3 bartender.py if using ROS Noetic'
 
 def main():
     bot = InterbotixManipulatorXS("wx250", "arm", "gripper")
+
+    if (bot.arm.group_info.num_joints < 5):
+        print('This demo requires the robot to have at least 5 joints!')
+        sys.exit()
+
     bot.arm.go_to_home_pose()
     bot.arm.set_ee_cartesian_trajectory(z=-0.2)
     bot.arm.set_ee_cartesian_trajectory(x=-0.2)
