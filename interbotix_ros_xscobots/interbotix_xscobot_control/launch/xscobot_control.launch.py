0e73f6d8b3bc01b790bc620b86c2b7cfaf716720
Luke Schmitt <luke@trossenrobotics.com>
2023-06-15 15:42:38 -0500

    [ros_xscobots] Add configs, URDF, launch files

---
+++ interbotix_ros_xscobots/interbotix_xscobot_control/launch/xscobot_control.launch.py
@@ -0,0 +1,229 @@
+# Copyright 2023 Trossen Robotics
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from interbotix_xs_modules.xs_common import (
+    get_interbotix_xscobot_models,
+)
+from interbotix_xs_modules.xs_launch import (
+    declare_interbotix_xscobot_robot_description_launch_arguments,
+)
+from interbotix_xs_modules.xs_launch.xs_launch import determine_use_sim_time_param
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import (
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def launch_setup(context, *args, **kwargs):
+
+    robot_model_launch_arg = LaunchConfiguration('robot_model')
+    robot_name_launch_arg = LaunchConfiguration('robot_name')
+    use_rviz_launch_arg = LaunchConfiguration('use_rviz')
+    motor_configs_launch_arg = LaunchConfiguration('motor_configs')
+    mode_configs_launch_arg = LaunchConfiguration('mode_configs')
+    load_configs_launch_arg = LaunchConfiguration('load_configs')
+    use_sim_launch_arg = LaunchConfiguration('use_sim')
+    robot_description_launch_arg = LaunchConfiguration('robot_description')
+    hardware_type_launch_arg = LaunchConfiguration('hardware_type')
+    xs_driver_logging_level_launch_arg = LaunchConfiguration('xs_driver_logging_level')
+
+    # sets use_sim_time parameter to 'true' if using gazebo hardware
+    use_sim_time_param = determine_use_sim_time_param(
+        context=context,
+        hardware_type_launch_arg=hardware_type_launch_arg
+    )
+
+    xscobot_description_launch_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('interbotix_xscobot_descriptions'),
+                'launch',
+                'xscobot_description.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'robot_model': robot_model_launch_arg,
+            'robot_name': robot_name_launch_arg,
+            'use_rviz': use_rviz_launch_arg,
+            'robot_description': robot_description_launch_arg,
+            'use_sim_time': use_sim_time_param,
+        }.items(),
+    )
+
+    xs_sdk_node = Node(
+        condition=UnlessCondition(use_sim_launch_arg),
+        package='interbotix_xs_sdk',
+        executable='xs_sdk',
+        name='xs_sdk',
+        namespace=robot_name_launch_arg,
+        arguments=[],
+        parameters=[{
+            'motor_configs': motor_configs_launch_arg,
+            'mode_configs': mode_configs_launch_arg,
+            'load_configs': load_configs_launch_arg,
+            'robot_description': robot_description_launch_arg,
+            'use_sim_time': use_sim_time_param,
+            'xs_driver_logging_level': xs_driver_logging_level_launch_arg,
+        }],
+        output={'both': 'screen'},
+    )
+
+    xs_sdk_sim_node = Node(
+        condition=IfCondition(use_sim_launch_arg),
+        package='interbotix_xs_sdk',
+        executable='xs_sdk_sim.py',
+        name='xs_sdk_sim',
+        namespace=robot_name_launch_arg,
+        arguments=[],
+        parameters=[{
+            'motor_configs': motor_configs_launch_arg,
+            'mode_configs': mode_configs_launch_arg,
+            'robot_description': robot_description_launch_arg,
+            'use_sim_time': use_sim_time_param,
+        }],
+        output={'both': 'screen'},
+    )
+
+    return [
+        xs_sdk_node,
+        xs_sdk_sim_node,
+        xscobot_description_launch_include,
+    ]
+
+
+def generate_launch_description():
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'robot_model',
+            choices=get_interbotix_xscobot_models(),
+            description='model type of the Interbotix Cobot such as `dx400`.'
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'robot_name',
+            default_value=LaunchConfiguration('robot_model'),
+            description=(
+                'name of the robot (typically equal to `robot_model`, but could be anything).'
+            ),
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_rviz',
+            default_value='true',
+            choices=('true', 'false'),
+            description='launches RViz if set to `true`.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'motor_configs',
+            default_value=[PathJoinSubstitution([
+                FindPackageShare('interbotix_xscobot_control'),
+                'config',
+                LaunchConfiguration('robot_model')]),
+                '.yaml'
+            ],
+            description="the file path to the 'motor config' YAML file.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'mode_configs',
+            default_value=PathJoinSubstitution([
+                FindPackageShare('interbotix_xscobot_control'),
+                'config',
+                'modes.yaml',
+            ]),
+            description="the file path to the 'mode config' YAML file.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'load_configs',
+            default_value='true',
+            choices=('true', 'false'),
+            description=(
+                'a boolean that specifies whether or not the initial register values '
+                "(under the 'motors' heading) in a Motor Config file should be written "
+                "to the motors; as the values being written are stored in each motor's "
+                'EEPROM (which means the values are retained even after a power cycle), '
+                'this can be set to `false` after the first time using the robot. Setting '
+                'to `false` also shortens the node startup time by a few seconds and '
+                'preserves the life of the EEPROM.'
+            ),
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'xs_driver_logging_level',
+            default_value='INFO',
+            choices=('DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'),
+            description='set the logging level of the X-Series Driver.'
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_sim',
+            default_value='false',
+            choices=('true', 'false'),
+            description=(
+                'if `true`, the DYNAMIXEL simulator node is run; use RViz to visualize the'
+                " robot's motion; if `false`, the real DYNAMIXEL driver node is run."
+            ),
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            choices=('true', 'false'),
+            description=(
+                'tells ROS nodes asking for time to get the Gazebo-published simulation time, '
+                'published over the ROS topic /clock; this value is automatically set to `true` if'
+                ' using Gazebo hardware.'
+            )
+        )
+    )
+    declared_arguments.extend(
+        declare_interbotix_xscobot_robot_description_launch_arguments()
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
---
+++ interbotix_ros_xscobots/interbotix_xscobot_descriptions/launch/xscobot_description.launch.py
@@ -0,0 +1,183 @@
+# Copyright 2023 Trossen Robotics
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from interbotix_xs_modules.xs_common import (
+    get_interbotix_xscobot_models,
+)
+from interbotix_xs_modules.xs_launch import (
+    declare_interbotix_xscobot_robot_description_launch_arguments,
+)
+from interbotix_xs_modules.xs_launch.xs_launch import determine_use_sim_time_param
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.conditions import IfCondition
+from launch.substitutions import (
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def launch_setup(context, *args, **kwargs):
+    robot_name_launch_arg = LaunchConfiguration('robot_name')
+    use_rviz_launch_arg = LaunchConfiguration('use_rviz')
+    use_joint_pub_launch_arg = LaunchConfiguration('use_joint_pub')
+    use_joint_pub_gui_launch_arg = LaunchConfiguration('use_joint_pub_gui')
+    rvizconfig_launch_arg = LaunchConfiguration('rvizconfig')
+    robot_description_launch_arg = LaunchConfiguration('robot_description')
+    hardware_type_launch_arg = LaunchConfiguration('hardware_type')
+
+    # sets use_sim_time parameter to 'true' if using gazebo hardware
+    use_sim_time_param = determine_use_sim_time_param(
+        context=context,
+        hardware_type_launch_arg=hardware_type_launch_arg
+    )
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{
+            'robot_description': robot_description_launch_arg,
+            'use_sim_time': use_sim_time_param,
+        }],
+        namespace=robot_name_launch_arg,
+        output={'both': 'log'},
+    )
+
+    joint_state_publisher_node = Node(
+        condition=IfCondition(use_joint_pub_launch_arg),
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        namespace=robot_name_launch_arg,
+        parameters=[{
+            'use_sim_time': use_sim_time_param,
+        }],
+        output={'both': 'log'},
+    )
+
+    joint_state_publisher_gui_node = Node(
+        condition=IfCondition(use_joint_pub_gui_launch_arg),
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        namespace=robot_name_launch_arg,
+        output={'both': 'log'},
+    )
+
+    rviz2_node = Node(
+        condition=IfCondition(use_rviz_launch_arg),
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        namespace=robot_name_launch_arg,
+        arguments=[
+            '-d', rvizconfig_launch_arg,
+        ],
+        parameters=[{
+            'use_sim_time': use_sim_time_param,
+        }],
+        output={'both': 'log'},
+    )
+
+    return [
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz2_node,
+    ]
+
+
+def generate_launch_description():
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'robot_model',
+            choices=get_interbotix_xscobot_models(),
+            description='model type of the Interbotix Cobot such as `dx400`.'
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'robot_name',
+            default_value=LaunchConfiguration('robot_model'),
+            description=(
+                'name of the robot (typically equal to `robot_model`, but could be anything).'
+            ),
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_rviz',
+            default_value='true',
+            choices=('true', 'false'),
+            description='launches RViz if set to `true`.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_joint_pub',
+            default_value='false',
+            choices=('true', 'false'),
+            description='launches the joint_state_publisher node.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_joint_pub_gui',
+            default_value='false',
+            choices=('true', 'false'),
+            description='launches the joint_state_publisher GUI.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'rvizconfig',
+            default_value=PathJoinSubstitution([
+                FindPackageShare('interbotix_xscobot_descriptions'),
+                'rviz',
+                'xscobot_description.rviz',
+            ]),
+            description='file path to the config file RViz should load.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            choices=('true', 'false'),
+            description=(
+                'tells ROS nodes asking for time to get the Gazebo-published simulation time, '
+                'published over the ROS topic /clock.'
+            )
+        )
+    )
+    declared_arguments.extend(
+        declare_interbotix_xscobot_robot_description_launch_arguments(),
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
