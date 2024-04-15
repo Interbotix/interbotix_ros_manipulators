000788216af824bdbb312c2e1c224cc5155392ee
Luke Schmitt <luke@trossenrobotics.com>
2024-04-15 19:45:29 +0000

    Minor linting

--- interbotix_ros_xsarms/interbotix_xsarm_control/demos/joint_current_control.py
+++ interbotix_ros_xsarms/interbotix_xsarm_control/demos/joint_current_control.py
@@ -31,7 +31,6 @@
 from interbotix_common_modules.common_robot.robot import robot_shutdown, robot_startup
 from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
 
-
 """
 This script commands currents [mA] to the arm joints
 
