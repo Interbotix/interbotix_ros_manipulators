a9c3ce1a5bb3b12f7fbea9daa428a0c3cfe9961e
Zhantao Song <zhantao2@illinois.edu>
2025-08-05 14:21:35 -0500

    Updated links to forked repo in install script

--- interbotix_ros_xsarms/install/rpi5/xsarm_rpi5_install.sh
+++ interbotix_ros_xsarms/install/rpi5/xsarm_rpi5_install.sh
@@ -294,9 +294,9 @@ function install_ros2() {
   else
     echo -e "${GRN}Installing ROS 2 packages for the Interbotix Arm...${OFF}"
     cd $INSTALL_PATH/src
-    git clone -b "$ROS_DISTRO_TO_INSTALL" https://github.com/Interbotix/interbotix_ros_core.git
-    git clone -b "$ROS_DISTRO_TO_INSTALL" https://github.com/Interbotix/interbotix_ros_manipulators.git
-    git clone -b "$ROS_DISTRO_TO_INSTALL" https://github.com/Interbotix/interbotix_ros_toolboxes.git
+    git clone -b "$ROS_DISTRO_TO_INSTALL" https://github.com/ZhantaoSong/interbotix_ros_core.git
+    git clone -b "$ROS_DISTRO_TO_INSTALL" https://github.com/ZhantaoSong/interbotix_ros_manipulators.git
+    git clone -b "$ROS_DISTRO_TO_INSTALL" https://github.com/ZhantaoSong/interbotix_ros_toolboxes.git
     # TODO(lsinterbotix) remove below when moveit_visual_tools is available in apt repo
     git clone -b ros2 https://github.com/ros-planning/moveit_visual_tools.git
     rm                                                                                                  \
